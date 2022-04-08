5882be251a8e56655a4657235753b1c3314f967c
LampardNK <nguyenkhackhanhlam@gmail.com>
2022-04-08 19:28:10 +0800

    suffering

--- cogs/login.py
+++ cogs/login.py
@@ -73,8 +73,7 @@ async def link(ctx,username,password):
 
                     cursor.execute(
                         'UPDATE codeforcer.creds'\
-                        'SET cf_handle = %s, cf_password = %s'\
-                        'WHERE discord_id = %s;',
+                        'SET cf_handle = %s, cf_password = %s WHERE discord_id = %s',
                         (
                             username,
                             password,
